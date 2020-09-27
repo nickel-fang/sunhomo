c8282792b2d6cdd043f3e80be241f81e6c21ab6f
unknown <nickel.fang79@gmail.com>
2020-09-27 22:06:38 +0800

    添加jks文件

--- sunhomo-java/club_admin/src/main/java/cn/sunhomo/club/controller/ActivityController.java
+++ sunhomo-java/club_admin/src/main/java/cn/sunhomo/club/controller/ActivityController.java
@@ -2,6 +2,8 @@ package cn.sunhomo.club.controller;
 
 import cn.sunhomo.club.domain.SunActivity;
 import cn.sunhomo.club.service.ISunActivityService;
+import cn.sunhomo.core.AjaxResult;
+import cn.sunhomo.core.TableDataInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,57 +17,57 @@ public class ActivityController {
     private String prefix = "club/activity";
     @Autowired
     private ISunActivityService activityService;
-//
-//    @GetMapping()
-//    public String list() {
-//        return prefix + "/activity";
-//    }
-//
-//    @PostMapping("/list")
-////    @RequiresPermissionsons("club:activity:list")
-//    @ResponseBody
-//    public TableDataInfo list(SunActivity activity) {
-//        startPage();
-//        List<SunActivity> activities = activityService.selectActivities(activity);
-//        return getDataTable(activities);
-//    }
-//
-//    @GetMapping("/add")
-//    public String add() {
-//        return prefix + "/add";
-//    }
-//
-////    @RequiresPermissions("club:activity:add")
-////    @Log(title = "活动管理", businessType = BusinessType.INSERT)
-//    @PostMapping("/add")
-//    @ResponseBody
-//    public AjaxResult addSave(SunActivity activity) {
-//        return toAjax(activityService.insertActivity(activity));
-//    }
-//
-////    @RequiresPermissions("club:activity:remove")
-////    @Log(title = "活动管理", businessType = BusinessType.DELETE)
-//    @PostMapping("/remove")
-//    @ResponseBody
-//    public AjaxResult remove(String ids) {
-//        try {
-//            return toAjax(activityService.deleteActivityById(ids));
-//        } catch (Exception e) {
-//            return error(e.getMessage());
-//        }
-//    }
-//
-//    @GetMapping("/edit/{id}")
-//    public String edit(@PathVariable("id") Integer activityId, ModelMap mmap) {
-//        mmap.put("activity", activityService.selectActivity(activityId));
-//        return prefix + "/edit";
-//    }
-//
-////    @RequiresPermissions("club:activity:edit")
-////    @Log(title = "活动管理", businessType = BusinessType.UPDATE)
-//    @PostMapping("/edit")
-//    @ResponseBody
-//    public AjaxResult editSave(SunActivity activity) {
-//        return toAjax(activityService.updateActivity(activity));
-//    }
+
+    @GetMapping()
+    public String list() {
+        return prefix + "/activity";
+    }
+
+    @PostMapping("/list")
+//    @RequiresPermissionsons("club:activity:list")
+    @ResponseBody
+    public TableDataInfo list(SunActivity activity) {
+        startPage();
+        List<SunActivity> activities = activityService.selectActivities(activity);
+        return getDataTable(activities);
+    }
+
+    @GetMapping("/add")
+    public String add() {
+        return prefix + "/add";
+    }
+
+    //    @RequiresPermissions("club:activity:add")
+//    @Log(title = "活动管理", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(SunActivity activity) {
+        return new AjaxResult(activityService.insertActivity(activity), null, null);
+    }
+
+    //    @RequiresPermissions("club:activity:remove")
+//    @Log(title = "活动管理", businessType = BusinessType.DELETE)
+    @PostMapping("/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids) {
+        try {
+            return new AjaxResult(activityService.deleteActivityById(ids), null, null);
+        } catch (Exception e) {
+            return new AjaxResult(500, e.getMessage(), null);
+        }
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer activityId, ModelMap mmap) {
+        mmap.put("activity", activityService.selectActivity(activityId));
+        return prefix + "/edit";
+    }
+
+    //    @RequiresPermissions("club:activity:edit")
+//    @Log(title = "活动管理", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(SunActivity activity) {
+        return new AjaxResult(activityService.updateActivity(activity), null, null);
+    }
 }
---
+++ sunhomo-java/club_framework/src/main/java/cn/sunhomo/core/TableDataInfo.java
@@ -0,0 +1,21 @@
+package cn.sunhomo.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableDataInfo {
+    //总记录数
+    private long total;
+    //列表数据
+    private List<?> rows;
+    //消息状态码
+    private int code;
+    //消息内容
+    private String msg;
+}
