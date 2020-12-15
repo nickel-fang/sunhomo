a064397cfe7a94d0493d3b68632ab795c9bbceb6
Nickel Fang <nickel.fang79@gmail.com>
2020-12-15 15:56:10 +0800

    使用button组件获取用户基本信息

--- sunhomo-miniProgram/app.js
+++ sunhomo-miniProgram/app.js
@@ -18,38 +18,41 @@ App({
             var code = res.data.code;
             if (code == -1) {
               this.globalData.openId = res.data.data;
-              //获取用户信息，并插入数据库
-              wx.getSetting({
-                success: res => {
-                  if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                    wx.getUserInfo({
-                      success: res => {
-                        // 可以将 res 发送到后台，插入会员
-                        var member = {};
-                        member.openid = this.globalData.openId;
-                        member.memberName = res.userInfo.nickName;
-                        member.memberPhoto = res.userInfo.avatarUrl;
-
-                        wx.request({
-                          url: this.globalData.APIUrl + '/club/member/insertMember',
-                          method: 'POST',
-                          data: member,
-                          success: res => {
-                            this.globalData.userInfo = res.data.data;
-                          }
-                        })
-
-                        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                        // 所以此处加入 callback 以防止这种情况
-                        if (this.userInfoReadyCallback) {
-                          this.userInfoReadyCallback(res)
-                        }
-                      }
-                    })
-                  }
-                }
+              wx.switchTab({
+                url: '/pages/my/my',
               })
+              // //获取用户信息，并插入数据库
+              // wx.getSetting({
+              //   success: res => {
+              //     if (res.authSetting['scope.userInfo']) {
+              //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+              //       wx.getUserInfo({
+              //         success: res => {
+              //           // 可以将 res 发送到后台，插入会员
+              //           var member = {};
+              //           member.openid = this.globalData.openId;
+              //           member.memberName = res.userInfo.nickName;
+              //           member.memberPhoto = res.userInfo.avatarUrl;
+
+              //           wx.request({
+              //             url: this.globalData.APIUrl + '/club/member/insertMember',
+              //             method: 'POST',
+              //             data: member,
+              //             success: res => {
+              //               this.globalData.userInfo = res.data.data;
+              //             }
+              //           })
+
+              //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              //           // 所以此处加入 callback 以防止这种情况
+              //           if (this.userInfoReadyCallback) {
+              //             this.userInfoReadyCallback(res)
+              //           }
+              //         }
+              //       })
+              //     }
+              //   }
+              // })
             } else if (code == 1) {
               this.globalData.userInfo = res.data.data;
             } else if (code == 500) {
--- sunhomo-miniProgram/pages/my/my.js
+++ sunhomo-miniProgram/pages/my/my.js
@@ -4,51 +4,70 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'comming soon!',
+    motto: '更多功能开发中...',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
+    var that = this;
     if (app.globalData.userInfo) {
-      this.setData({
+      that.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (that.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+        // this.setData({
+        //   userInfo: res.userInfo,
+        //   hasUserInfo: true
+        // })
+        that.insertMember(res.userInfo);
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+          // app.globalData.userInfo = res.userInfo
+          // this.setData({
+          //   userInfo: res.userInfo,
+          //   hasUserInfo: true
+          // })
+          that.insertMember(res.userInfo);
         }
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  getUserInfo: function (e) {
+    // console.log(e)
+    // app.globalData.userInfo = e.detail.userInfo
+    // this.setData({
+    //   userInfo: e.detail.userInfo,
+    //   hasUserInfo: true
+    // })
+    this.insertMember(e.detail.userInfo);
+  },
+  insertMember: function (userInfo) {
+    var that = this;
+    var member = {};
+    member.openid = app.globalData.openId;
+    member.memberName = userInfo.nickName;
+    member.memberPhoto = userInfo.avatarUrl;
+
+    wx.request({
+      url: app.globalData.APIUrl + '/club/member/insertMember',
+      method: 'POST',
+      data: member,
+      success: res => {
+        app.globalData.userInfo = res.data.data;
+        that.setData({
+          userInfo: res.data.data,
+          hasUserInfo: true
+        })
+      }
+    });
   }
 })
